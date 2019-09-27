ef2255e65f1ab2d7028d9673d4642a0ec96d0bb0
Fride Skarseth <fs@computas.com>
2019-09-27 14:08:13 +0200

    Trying to add animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { CardContainerComponent } from './card-container/card-container.component';
 import { CardComponent } from './card-container/card/card.component';
@@ -18,7 +20,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card-container/card-container.component.ts
+++ src/app/card-container/card-container.component.ts
@@ -1,11 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import {
+	trigger,
+	state,
+	style,
+	transition,
+	animate,
+  keyframes,
+  query,
+  stagger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-card-container',
   templateUrl: './card-container.component.html',
-  styleUrls: ['./card-container.component.scss']
+  styleUrls: ['./card-container.component.scss'],
+  animations: [
+    trigger('scaling', [
+			state('active', style({
+				transform: 'translateX(0)'
+			})),
+			transition('void => *', [
+				animate(1500, keyframes([
+					style({
+						transform: 'scale(0.90)',
+						offset: 0.1
+					}),
+					style({
+						transform: 'scale(0.92)',
+						offset: 0.2
+					}),
+					style({
+						transform: 'scale(0.94)',
+						offset: 0.3
+					}),
+					style({
+						transform: 'scale(1)',
+						offset: 1
+					})
+				]))
+			]),
+    ]),
+    trigger('bump', [
+			state('void', style({
+        transform: 'translateY(300px)',
+        opacity: 0
+			})),
+			transition('void => *', [
+        animate(2000, keyframes([
+          style({ transform: 'translateY(40px)', opacity: 0.4, offset: 0.2 }),
+          style({ transform: 'translateY(0px)', opacity: 1, offset: 1 })
+        ]))
+      ]),
+    ]),
+    trigger('bump2', [
+			state('void', style({
+        transform: 'translateY(300px)',
+        opacity: 0
+			})),
+      transition('* => slideIn', [
+        query('.card', stagger('100ms',
+          animate(1500, keyframes([
+            style({ transform: 'translateY(40px)', opacity: 0.4, offset: 0.2}),
+            style({ transform: 'translateY(0px)', opacity: 1, offset: 1 })
+          ]))
+        )),
+      ]),
+		]),
+  ]
 })
 export class CardContainerComponent implements OnInit {
+
+  public active: boolean = true;
 
   constructor() { }
 
